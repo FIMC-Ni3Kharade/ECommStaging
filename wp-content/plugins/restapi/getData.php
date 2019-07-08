a0513bdd5394711c8a704f3675943d11c3f6f3a5
Nitin Kharade <nkharade@fimc.com>
2019-07-08 05:35:12 -0400

    added test

---
+++ wp-content/plugins/restapi/getData.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name: Thanks for reading
+ * Plugin URI: http://www.mywebsite.com/my-first-plugin
+ * Description: The very first plugin that I have ever created.
+ * Version: 1.0
+ * Author: Kaustubh Patil
+ * Author URI: http://www.mywebsite.com
+ */
+ function my_scripts_method() {
+	wp_enqueue_script(
+		'jsscript',
+		plugins_url( '/js/jquery.js' , __FILE__ ),
+		array( 'jquery' )
+	  );
+	}
+	add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
+ 
+add_action( 'the_content', 'my_thank_you_text' );	
+function my_thank_you_text ( $content ) {
+//  $api = wp_remote_get("https://reqres.in/api/users");
+//     $json_api = json_decode(stripslashes($api['body']));
+// 	foreach($json_api->data as $row){
+// 		$content .=  '<br/>id: '.$row->id.' email:'.$row->email.' first_name:'.$row->first_name.' last_name:'.$row->last_name;
+// 	}
+		$content.='<button class="btnServiceCall">Call Service</button><div class="serviceData"></div>';	
+    return $content;
+}
+
+
+function console_log($output, $with_script_tags = true) {
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+');';
+    if ($with_script_tags) {
+        $js_code = '<script>' . $js_code . '</script>';
+    }
+    echo $js_code;
+}
+
+?>
---
+++ wp-content/plugins/restapi/js/jquery.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function ($) {
+
+    // $ Works! You can test it with next line if you like
+    console.log('scc');
+
+
+    $(document).on('click', '.btnServiceCall', function () {
+        var btn=this;
+        $.get("https://reqres.in/api/users", function (response) {
+            $content=$(btn).siblings('.serviceData');
+            $content.html('All service data');
+            for (var i = 0; i < response.data.length; i++) {
+                var row=response.data[i];
+                $content.append('<br/>id: '+row.id+' email: '+row.email+' first_name: '+row.first_name+' last_name: '+row.last_name);
+             }
+        });
+    });
+
+});
