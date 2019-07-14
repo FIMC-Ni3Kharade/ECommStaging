271f067cdb4e42ee3b63dfc35d44b78a0c4e80ad
Nitin Kharade <nkharade@fimc.com>
2019-07-14 12:52:00 -0400

    test
    test

--- wp-content/plugins/restapi/getData.php
+++ wp-content/plugins/restapi/getData.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name: Thanks for reading
+ * Plugin Name: Login Form
  * Plugin URI: http://www.mywebsite.com/my-first-plugin
- * Description: The very first plugin that I have ever created.
+ * Description: Login Form
  * Version: 1.0
- * Author: Kaustubh Patil
+ * Author: Nitin Kharade
  * Author URI: http://www.mywebsite.com
  */
  function my_scripts_method() {
@@ -23,8 +23,8 @@ function my_thank_you_text ( $content ) {
 // 	foreach($json_api->data as $row){
 // 		$content .=  '<br/>id: '.$row->id.' email:'.$row->email.' first_name:'.$row->first_name.' last_name:'.$row->last_name;
 // 	}
-		$content.='<button id="btnServiceCall" class="et_pb_button et_pb_button_0 et_pb_bg_layout_light">Call Service</button><div class="serviceData"></div>';	
-    return $content;
+	//$content .= '<button id="btnServiceCall" class="et_pb_button et_pb_button_0 et_pb_bg_layout_light">Call Service</button><div class="serviceData"></div>';	
+	//return $content;
 }
 
 
@@ -38,3 +38,30 @@ function console_log($output, $with_script_tags = true) {
 }
 
 ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+</head>
+<body>
+<form id="login_form" action="" method="post">
+	<fieldset>
+		<p>
+			<label for="user_Login">Username</label>
+			<input name="user_login" id="txtUserName" class="required" type="text"/>
+		</p>
+		<p>
+			<label for="user_pass">Password</label>
+			<input name="user_pass" id="txtPassword" class="required" type="password"/>
+		</p>
+		<p>
+			<input type="hidden" name="login_nonce" value=""/>
+			<input id="btnServiceCall" type="button" value="Login" />
+		</p>
+		<p>
+			<span class="lblErrMsg"></span>
+		</p>
+	</fieldset>
+</form>
+</body>
+</html>
--- wp-content/plugins/restapi/js/jquery.js
+++ wp-content/plugins/restapi/js/jquery.js
@@ -5,15 +5,65 @@ jQuery(document).ready(function ($) {
 
 
     $(document).on('click', '#btnServiceCall', function () {
-        var btn=this;
-        $.get("https://reqres.in/api/users", function (response) {
-            $content=$(btn).siblings('.serviceData');
-            $content.html('All service data');
-            for (var i = 0; i < response.data.length; i++) {
-                var row=response.data[i];
-                $content.append('<br/>id: '+row.id+' email: '+row.email+' first_name: '+row.first_name+' last_name: '+row.last_name);
-             }
-        });
+        var btn = this;
+        LoginBranch();
+        //$.get("https://reqres.in/api/users", function (response) {
+        //    $content=$(btn).siblings('.serviceData');
+        //    $content.html('All service data');
+        //    for (var i = 0; i < response.data.length; i++) {
+        //        var row=response.data[i];
+        //        $content.append('<br/>id: '+row.id+' email: '+row.email+' first_name: '+row.first_name+' last_name: '+row.last_name);
+        //     }
+        //});
     });
+
+
+
+    function LoginBranch() {
+        var userName = $("#txtUserName").val();
+        var pwd = $("#txtPassword").val();
+        var dataParam = { "AppUrl": "http://betaplus.homeandauto.com" };
+        var vdata = JSON.stringify(dataParam);
+        $.ajax({
+            type: "POST",
+            url: "http://10.10.50.176/ServiceLayer/ServiceLayer/Auth/GetAuthUserIdentifier",
+            contentType: "application/json; charset=utf-8",
+            data: vdata,
+            cache: false,
+            dataType: "json",
+            async: true,
+
+            beforeSend: function setHeader(xhr) {
+                xhr.setRequestHeader('Authorization', 'Basic ' + btoa(userName + ':' + pwd));
+            },
+            success: function (data, textStatus, request) {
+                onSuccessToken(data);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                onErrorToken(XMLHttpRequest, textStatus, errorThrown);
+            }
+        });
+    }
+
+
+    function onSuccessToken(tokenData) {
+        try {
+            if (tokenData.length != 0) {
+                alert(tokenData.UserTokenID);
+            }
+            else {
+                $('.lblErrMsg').html('Something went wrong');
+            }
+        } catch (e) {
+        }
+    }
+
+    function onErrorToken(XMLHttpRequest, textStatus, errorThrown, inputData) {
+        try {
+            $('.lblErrMsg').html('Invalid login attempt.');
+        } catch (e) {
+        }
+    }
+
 
 });
